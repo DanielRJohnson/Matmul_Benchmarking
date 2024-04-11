61adb4b0edf19b30632c2ff6509e783d0a3d9434
ephing <ethangrantz201@gmail.com>
2024-04-11 11:02:38 -0500

    DAC_notemp finished

--- Matmul_Benchmarking/algorithms.cpp
+++ Matmul_Benchmarking/algorithms.cpp
@@ -39,11 +39,7 @@ void tiledMatmul(const Matrix& A, const Matrix& B, Matrix& C, int tileSize) {
 }
 
 void tempDACMatmul(const Matrix& A, const Matrix& B, Matrix& C, int sizeCutoff) {
-	_tempDACMatmulInner(A, B, C, C.innerSize(), sizeCutoff);
-}
-
-void _tempDACMatmulInner(const Matrix& A, const Matrix& B, Matrix& C, int size, int sizeCutoff) {
-	if ( sizeCutoff == size ) {
+	if ( sizeCutoff == C.rowSize() ) {
 		vanillaMatmul(A, B, C, 0);
 	} else {
 		auto T = generateMatrix(C.innerSize(), false);
@@ -78,35 +74,35 @@ void _tempDACMatmulInner(const Matrix& A, const Matrix& B, Matrix& C, int size, 
 		{
 			#pragma omp section
 			{ 
-				_tempDACMatmulInner(a11, b11, c11, size/2, sizeCutoff);
+				tempDACMatmul(a11, b11, c11, sizeCutoff);
 			}
 			#pragma omp section
 			{ 
-				_tempDACMatmulInner(a11, b12, c12, size/2, sizeCutoff);
+				tempDACMatmul(a11, b12, c12, sizeCutoff);
 			}
 			#pragma omp section
 			{ 
-				_tempDACMatmulInner(a21, b11, c21, size/2, sizeCutoff);
+				tempDACMatmul(a21, b11, c21, sizeCutoff);
 			}
 			#pragma omp section
 			{ 
-				_tempDACMatmulInner(a21, b12, c22, size/2, sizeCutoff);
+				tempDACMatmul(a21, b12, c22, sizeCutoff);
 			}
 			#pragma omp section
 			{ 
-				_tempDACMatmulInner(a12, b21, t11, size/2, sizeCutoff);
+				tempDACMatmul(a12, b21, t11, sizeCutoff);
 			}
 			#pragma omp section
 			{ 
-				_tempDACMatmulInner(a12, b22, t12, size/2, sizeCutoff);
+				tempDACMatmul(a12, b22, t12, sizeCutoff);
 			}
 			#pragma omp section
 			{ 
-				_tempDACMatmulInner(a22, b21, t21, size/2, sizeCutoff);
+				tempDACMatmul(a22, b21, t21, sizeCutoff);
 			}
 			#pragma omp section
 			{ 
-				_tempDACMatmulInner(a22, b22, t22, size/2, sizeCutoff);
+				tempDACMatmul(a22, b22, t22, sizeCutoff);
 			}
 		}
 
@@ -123,7 +119,70 @@ void _tempDACMatmulInner(const Matrix& A, const Matrix& B, Matrix& C, int size, 
 }
 
 void noTempDACMatmul(const Matrix& A, const Matrix& B, Matrix& C, int sizeCutoff) {
-	
+	if ( C.rowSize() == sizeCutoff ) {
+		vanillaMatmul(A, B, C, 0);
+	} else {
+		std::size_t rowPivotA = A.rowLowerBound() + (A.rowSize()/2);
+		std::size_t colPivotA = A.colLowerBound() + (A.colSize()/2);
+		std::size_t rowPivotB = B.rowLowerBound() + (B.rowSize()/2);
+		std::size_t colPivotB = B.colLowerBound() + (B.colSize()/2);
+		std::size_t rowPivotC = C.rowLowerBound() + (C.rowSize()/2);
+		std::size_t colPivotC = C.colLowerBound() + (C.colSize()/2);
+
+		// submatrices
+		auto a11 = A.subMatrix(A.rowLowerBound(), rowPivotA, A.colLowerBound(), colPivotA);
+		auto a12 = A.subMatrix(A.rowLowerBound(), rowPivotA, colPivotA, A.colUpperBound());
+		auto a21 = A.subMatrix(rowPivotA, A.rowUpperBound(), A.colLowerBound(), colPivotA);
+		auto a22 = A.subMatrix(rowPivotA, A.rowUpperBound(), colPivotA, A.colUpperBound());
+		auto b11 = B.subMatrix(B.rowLowerBound(), rowPivotB, B.colLowerBound(), colPivotB);
+		auto b12 = B.subMatrix(B.rowLowerBound(), rowPivotB, colPivotB, B.colUpperBound());
+		auto b21 = B.subMatrix(rowPivotB, B.rowUpperBound(), B.colLowerBound(), colPivotB);
+		auto b22 = B.subMatrix(rowPivotB, B.rowUpperBound(), colPivotB, B.colUpperBound());
+		auto c11 = C.subMatrix(C.rowLowerBound(), rowPivotC, C.colLowerBound(), colPivotC);
+		auto c12 = C.subMatrix(C.rowLowerBound(), rowPivotC, colPivotC, C.colUpperBound());
+		auto c21 = C.subMatrix(rowPivotC, C.rowUpperBound(), C.colLowerBound(), colPivotC);
+		auto c22 = C.subMatrix(rowPivotC, C.rowUpperBound(), colPivotC, C.colUpperBound());
+
+		#pragma omp parallel sections
+		{
+			#pragma omp section
+			{ 
+				tempDACMatmul(a11, b11, c11, sizeCutoff);
+			}
+			#pragma omp section
+			{ 
+				tempDACMatmul(a11, b12, c12, sizeCutoff);
+			}
+			#pragma omp section
+			{ 
+				tempDACMatmul(a21, b11, c21, sizeCutoff);
+			}
+			#pragma omp section
+			{ 
+				tempDACMatmul(a21, b12, c22, sizeCutoff);
+			}
+		}
+
+		#pragma omp parallel sections
+		{
+			#pragma omp section
+			{ 
+				tempDACMatmul(a12, b21, c11, sizeCutoff);
+			}
+			#pragma omp section
+			{ 
+				tempDACMatmul(a12, b22, c12, sizeCutoff);
+			}
+			#pragma omp section
+			{ 
+				tempDACMatmul(a22, b21, c21, sizeCutoff);
+			}
+			#pragma omp section
+			{ 
+				tempDACMatmul(a22, b22, c22, sizeCutoff);
+			}
+		}
+	}
 }
 
 void strassensMatmul(const Matrix& A, const Matrix& B, Matrix& C, int sizeCutoff) {
