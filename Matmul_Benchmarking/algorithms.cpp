8f6684b58643abe20603aa4bd10b9306097d8ef6
Daniel Johnson <danielrjohnson00@gmail.com>
2024-04-18 12:43:38 -0500

    change to omp tasks and upload new data

--- Matmul_Benchmarking/algorithms.cpp
+++ Matmul_Benchmarking/algorithms.cpp
@@ -19,10 +19,12 @@ void vanillaMatmul(const Matrix& A, const Matrix& B, Matrix& C, int placeholder)
 void _vanillaAdjusted(const Matrix& A, const Matrix& B, Matrix& C) {
 	/*
 	* Performs a standard matrix multiplication. 
-	* Strassens and DAC_temp can have C appear in different offsets, which this version accounts for
+	* tiled, DAC, and Strassens can have C appear in different offsets, which this version accounts for
 	*/
-	std::size_t i, j;
+	int i, j;
+	#pragma omp parallel for
 	row_iter(A, i) {
+		#pragma omp parallel for
 		col_iter(B, j) {
 			for ( auto k = 0; k < B.rowSize(); k++ ) {
 				C[i-A.rowLowerBound()+C.rowLowerBound()][j-B.colLowerBound()+C.colLowerBound()] 
@@ -103,39 +105,43 @@ void tempDACMatmul(const Matrix& A, const Matrix& B, Matrix& C, int sizeCutoff) 
 		auto t22 = T.subMatrix(rowPivotT, C.rowSize(), colPivotT, C.colSize());
 
 		// recurse
-		#pragma omp parallel sections
+		#pragma omp parallel
 		{
-			#pragma omp section
-			{ 
-				tempDACMatmul(a11, b11, c11, sizeCutoff);
-			}
-			#pragma omp section
-			{ 
-				tempDACMatmul(a11, b12, c12, sizeCutoff);
-			}
-			#pragma omp section
-			{ 
-				tempDACMatmul(a21, b11, c21, sizeCutoff);
-			}
-			#pragma omp section
-			{ 
-				tempDACMatmul(a21, b12, c22, sizeCutoff);
-			}
-			#pragma omp section
-			{ 
-				tempDACMatmul(a12, b21, t11, sizeCutoff);
-			}
-			#pragma omp section
-			{ 
-				tempDACMatmul(a12, b22, t12, sizeCutoff);
-			}
-			#pragma omp section
-			{ 
-				tempDACMatmul(a22, b21, t21, sizeCutoff);
-			}
-			#pragma omp section
-			{ 
-				tempDACMatmul(a22, b22, t22, sizeCutoff);
+			#pragma omp single nowait 
+			{
+				#pragma omp task
+				{
+					tempDACMatmul(a11, b11, c11, sizeCutoff);
+				}
+				#pragma omp task
+				{
+					tempDACMatmul(a11, b12, c12, sizeCutoff);
+				}
+				#pragma omp task
+				{
+					tempDACMatmul(a21, b11, c21, sizeCutoff);
+				}
+				#pragma omp task
+				{
+					tempDACMatmul(a21, b12, c22, sizeCutoff);
+				}
+				#pragma omp task
+				{
+					tempDACMatmul(a12, b21, t11, sizeCutoff);
+				}
+				#pragma omp task
+				{
+					tempDACMatmul(a12, b22, t12, sizeCutoff);
+				}
+				#pragma omp task
+				{
+					tempDACMatmul(a22, b21, t21, sizeCutoff);
+				}
+				#pragma omp task
+				{
+					tempDACMatmul(a22, b22, t22, sizeCutoff);
+				}
+				#pragma omp taskwait
 			}
 		}
 
@@ -176,43 +182,51 @@ void noTempDACMatmul(const Matrix& A, const Matrix& B, Matrix& C, int sizeCutoff
 		auto c21 = C.subMatrix(rowPivotC, C.rowUpperBound(), C.colLowerBound(), colPivotC);
 		auto c22 = C.subMatrix(rowPivotC, C.rowUpperBound(), colPivotC, C.colUpperBound());
 
-		#pragma omp parallel sections
+		#pragma omp parallel
 		{
-			#pragma omp section
-			{ 
-				noTempDACMatmul(a11, b11, c11, sizeCutoff);
-			}
-			#pragma omp section
-			{ 
-				noTempDACMatmul(a11, b12, c12, sizeCutoff);
-			}
-			#pragma omp section
-			{ 
-				noTempDACMatmul(a21, b11, c21, sizeCutoff);
-			}
-			#pragma omp section
-			{ 
-				noTempDACMatmul(a21, b12, c22, sizeCutoff);
+			#pragma omp single nowait
+			{
+				#pragma omp task
+				{
+					noTempDACMatmul(a11, b11, c11, sizeCutoff);
+				}
+				#pragma omp task
+				{
+					noTempDACMatmul(a11, b12, c12, sizeCutoff);
+				}
+				#pragma omp task
+				{
+					noTempDACMatmul(a21, b11, c21, sizeCutoff);
+				}
+				#pragma omp task
+				{
+					noTempDACMatmul(a21, b12, c22, sizeCutoff);
+				}
+				#pragma omp taskwait
 			}
 		}
 
-		#pragma omp parallel sections
+		#pragma omp parallel
 		{
-			#pragma omp section
-			{ 
-				noTempDACMatmul(a12, b21, c11, sizeCutoff);
-			}
-			#pragma omp section
-			{ 
-				noTempDACMatmul(a12, b22, c12, sizeCutoff);
-			}
-			#pragma omp section
-			{ 
-				noTempDACMatmul(a22, b21, c21, sizeCutoff);
-			}
-			#pragma omp section
-			{ 
-				noTempDACMatmul(a22, b22, c22, sizeCutoff);
+			#pragma omp single nowait
+			{
+				#pragma omp task
+				{
+					noTempDACMatmul(a12, b21, c11, sizeCutoff);
+				}
+				#pragma omp task
+				{
+					noTempDACMatmul(a12, b22, c12, sizeCutoff);
+				}
+				#pragma omp task
+				{
+					noTempDACMatmul(a22, b21, c21, sizeCutoff);
+				}
+				#pragma omp task
+				{
+					noTempDACMatmul(a22, b22, c22, sizeCutoff);
+				}
+				#pragma omp taskwait
 			}
 		}
 	}
@@ -277,105 +291,117 @@ void strassensMatmul(const Matrix& A, const Matrix& B, Matrix& C, int sizeCutoff
 		auto p4 = generateMatrix(c11.rowSize(), false);
 		auto p5 = generateMatrix(c11.rowSize(), false);
 
-		#pragma omp parallel sections
+		#pragma omp parallel
 		{
-			#pragma omp section
+			#pragma omp single nowait
 			{
-				matAdd(a11, a22, s0, false);
-			}
-			#pragma omp section
-			{
-				matAdd(b11, b22, s1, false);
-			}
-			#pragma omp section
-			{
-				matAdd(a21, a22, s2, false);
-			}
-			#pragma omp section
-			{
-				matAdd(b12, b22, s3, true);
-			}
-			#pragma omp section
-			{
-				matAdd(b21, b11, s4, true);
-			}
-			#pragma omp section
-			{
-				matAdd(a11, a12, s5, false);
-			}
-			#pragma omp section
-			{
-				matAdd(a21, a11, s6, true);
-			}
-			#pragma omp section
-			{
-				matAdd(b11, b12, s7, false);
-			}
-			#pragma omp section
-			{
-				matAdd(a12, a22, s8, true);
-			}
-			#pragma omp section
-			{
-				matAdd(b21, b22, s9, false);
+				#pragma omp task
+				{
+					matAdd(a11, a22, s0, false);
+				}
+				#pragma omp task
+				{
+					matAdd(b11, b22, s1, false);
+				}
+				#pragma omp task
+				{
+					matAdd(a21, a22, s2, false);
+				}
+				#pragma omp task
+				{
+					matAdd(b12, b22, s3, true);
+				}
+				#pragma omp task
+				{
+					matAdd(b21, b11, s4, true);
+				}
+				#pragma omp task
+				{
+					matAdd(a11, a12, s5, false);
+				}
+				#pragma omp task
+				{
+					matAdd(a21, a11, s6, true);
+				}
+				#pragma omp task
+				{
+					matAdd(b11, b12, s7, false);
+				}
+				#pragma omp task
+				{
+					matAdd(a12, a22, s8, true);
+				}
+				#pragma omp task
+				{
+					matAdd(b21, b22, s9, false);
+				}
+				#pragma omp taskwait
 			}
 		}
 
-		#pragma omp parallel sections
+		#pragma omp parallel
 		{
-			#pragma omp section
+			#pragma omp single nowait
 			{
-				strassensMatmul(s0, s1, p1, sizeCutoff);
-			}
-			#pragma omp section
-			{
-				strassensMatmul(s2, b11, p2, sizeCutoff);
-			}
-			#pragma omp section
-			{
-				strassensMatmul(a11, s3, p3, sizeCutoff);
-			}
-			#pragma omp section
-			{
-				strassensMatmul(a22, s4, p4, sizeCutoff);
-			}
-			#pragma omp section
-			{
-				strassensMatmul(s5, b22, p5, sizeCutoff);
-			}
-			#pragma omp section
-			{
-				strassensMatmul(s6, s7, c22, sizeCutoff);
-			}
-			#pragma omp section
-			{
-				strassensMatmul(s8, s9, c11, sizeCutoff);
+				#pragma omp task
+				{
+					strassensMatmul(s0, s1, p1, sizeCutoff);
+				}
+				#pragma omp task
+				{
+					strassensMatmul(s2, b11, p2, sizeCutoff);
+				}
+				#pragma omp task
+				{
+					strassensMatmul(a11, s3, p3, sizeCutoff);
+				}
+				#pragma omp task
+				{
+					strassensMatmul(a22, s4, p4, sizeCutoff);
+				}
+				#pragma omp task
+				{
+					strassensMatmul(s5, b22, p5, sizeCutoff);
+				}
+				#pragma omp task
+				{
+					strassensMatmul(s6, s7, c22, sizeCutoff);
+				}
+				#pragma omp task
+				{
+					strassensMatmul(s8, s9, c11, sizeCutoff);
+				}
+				#pragma omp taskwait
 			}
 		}
 
-		#pragma omp parallel sections
+		#pragma omp parallel
 		{
-			#pragma omp section
+			#pragma omp single nowait
 			{
-				matAdd(c11, p1, c11, false);
-				matAdd(c11, p4, c11, false);
-				matAdd(c11, p5, c11, true);
-			}
-			#pragma omp section
-			{
-				matAdd(c12, p3, c12, false);
-				matAdd(c12, p5, c12, false);
-			}
-			#pragma omp section
-			{
-				matAdd(c21, p4, c21, false);
-				matAdd(c21, p2, c21, false);
-			}
-			#pragma omp section
-			{
-				matAdd(c22, p1, c22, false);
-				matAdd(c22, p2, c22, true);
-				matAdd(c22, p3, c22, false);
+				#pragma omp task
+				{
+					matAdd(c11, p1, c11, false);
+					matAdd(c11, p4, c11, false);
+					matAdd(c11, p5, c11, true);
+				}
+				#pragma omp task
+				{
+					matAdd(c12, p3, c12, false);
+					matAdd(c12, p5, c12, false);
+				}
+				#pragma omp task
+				{
+					matAdd(c21, p4, c21, false);
+					matAdd(c21, p2, c21, false);
+				}
+				#pragma omp task
+				{
+					matAdd(c22, p1, c22, false);
+					matAdd(c22, p2, c22, true);
+					matAdd(c22, p3, c22, false);
+				}
+				#pragma omp taskwait
 			}
 		}
 	}
